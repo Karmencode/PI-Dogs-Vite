e023cfc79eb35193e6608f2ea62c9b5d73a5088c
Karmen <carmen.mendez.aguirre@gmail.com>
2023-10-18 22:31:58 -0600

    Primer cambio en estilos generales

--- client/src/components/landing page/Landing.jsx
+++ client/src/components/landing page/Landing.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Landing() {
     return (
         <div className={style.divMain}>
-            <div className={style.divText}>
+            <div className={style.divStyles}>
                 <div className={style.divTextos}>
                     <h1>Do you like dogs?</h1>
                     <h2>This is the place for you</h2>
@@ -15,10 +15,16 @@ function Landing() {
                     <Link to='/home'>
                         <button>Lets Go! 🐶</button>
                     </Link>
-                </div>
+                    <div className={style.divImages}>
+                        <img src='https://hips.hearstapps.com/hmg-prod/images/gettyimages-695480884-64f8446a4e85d.jpg' alt="1" />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzaS0EwJ--inZ2UfnDJoxfZOdDPVFuW01v-g&usqp=CAU" alt="2" />
+                        <img src="https://www.cuerpomente.com/medio/2023/06/08/perro-en-un-prado_bc5b1a31_230608141116_1280x720.jpg" alt="3" />
+                        <img src="https://hips.hearstapps.com/hmg-prod/images/portrait-of-labradoodle-with-humorous-expression-royalty-free-image-690356759-1542399719.jpg?crop=0.668xw:1.00xh;0.332xw,0&resize=1200:*" alt="4" />
+                        <img src="https://www.mdzol.com/u/fotografias/m/2023/2/1/f768x1-1356722_1356849_5050.jpg" alt="5" />
 
+                    </div>
+                </div>
             </div>
-            <div className={style.divImage}> </div>
         </div>
     )
 }
